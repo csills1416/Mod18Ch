b29b461cb27817baf2bf648bbce16e824853831e
CSillus <71670415+csills1416@users.noreply.github.com>
2023-09-21 17:51:59 -0400

    created data.js with fake accounts

---
+++ utils/data.js
@@ -0,0 +1,65 @@
+const users = [
+    {
+      username: 'admin',
+      email: 'admin@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'dingusdongus22',
+      email: 'dingus22@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'dingading23',
+      email: 'dingading23@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'bingcross21',
+      email: 'bingcross21@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'bingbingbing44',
+      email: 'bing44@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'idontandcant',
+      email: 'idontandcant@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'somethingelse11',
+      email: 'something11@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'alienlife55',
+      email: 'alien55@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'ufo000',
+      email: 'ufo000@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'biolegs44',
+      email: 'biolegs44@example.com',
+      thoughts: [],
+      friends: [],
+    },
+  ];
+  
+  module.exports = { users };
+  
